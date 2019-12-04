bf508b2d9a1d09705c6cd014de3d2b09e8cc0342
pninagugig <57224077+pninagugig@users.noreply.github.com>
2019-12-04 16:31:51 +0200

    Add files via upload

---
+++ example.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+const Header = (props) => {
+  console.log(props)
+  return <h1>{props.course}</h1>
+}
+const Content=(propers)=>{
+  return (props.parts.map(i=> <Part name={i.name} exercises={i.exercises} />))
+}
+const Total = (parts) => 
+{
+  return(<p>Number of exercises {parts.exercises1 + parts.exercises2 + parts.exercises3}</p>)
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+
+  return (
+    <div>
+     <Header course={course} />
+      <Content parts={partse}/>
+      <Total parts={partse}/>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
